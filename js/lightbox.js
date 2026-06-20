56e7ea6e64dcf32268dd60d91dba93e6b185c2ca
lunartransit64 <Thomas.roberts23@outlook.com>
2026-06-20 17:38:55 +1000

    added click function and text

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,8 +1,17 @@
 // Open Lightbox
-function OpenLightbox() {
-  // Show the Lightbox
-  document.getElementById("lightbox").style.display = "block";
-}
+document.addEventListener("click", function(event) {
+  // If click is on an image
+  if (event.target.TagName === "IMG" && event.target.id !== "lightbox-img") {
+    // Show the Lightbox
+    document.getElementById("lightbox").style.display = "block";
+
+    // Show clicked image as Lightbox image.
+    document.getElementById("lightbox-img".src = event.target.src;
+
+    // Show Alt text as description
+    document.getElementById("lightbox-txt".textContent = event.target.alt;
+  }
+});
 
 // Close Lightbox
 function CloseLightbox() {
