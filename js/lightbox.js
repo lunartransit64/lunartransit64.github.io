04ab9de5fc37761b75854471a08ffb8125951340
lunartransit64 <Thomas.roberts23@outlook.com>
2026-06-20 17:13:17 +1000

    lightbox start

---
+++ js/lightbox.js
@@ -0,0 +1,10 @@
+// Open Lightbox
+function OpenLightbox() {
+  // Show the Lightbox
+  document.getElementById("lightbox").style.display = "block";
+}
+
+// Close Lightbox
+function CloseLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
