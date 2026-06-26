4d9b2f38eaa14f6b435fffdf23b3f36ca72e6cf7
Lunar Transit <Thomas.roberts23@outlook.com>
2026-06-26 23:14:01 +1000

    started blogs.js

---
+++ js/blogs.js
@@ -0,0 +1,28 @@
+const dropdown = document.getElementById('sort-by');
+const bloglist = document.querySelector('.linkslist');
+
+dropdown.addEventListener('change', (event) => {
+  const value = event.target.value;
+
+  const links  Array.from(bloglist.querySelectorAll('a'));
+
+  links.sort((a, b) => {
+    // Find date of text
+    const dateA = a.InnerText.split(' - ');
+    const dateB = a.InnerText.split(' - ');
+    const JdateA = convertToDate(dateA);
+    const JdateB = convertToDate(dateB);
+
+    // Sort through Dates
+    if (value === 'newtoold') {
+      return JdateB - JdateA;
+    } else {
+      return JdateA - JdateB;
+    }
+  });
+
+  // Show on page
+  links.fromEach(link = > {
+    bloglist.appendChild(link);
+  });
+});
