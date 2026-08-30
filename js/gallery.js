f2ed3537ae8e90a5b3126da974d7fd583568309e
Lunar Transit <Thomas.roberts23@outlook.com>
2026-08-30 20:26:26 +1000

    added gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,27 @@
+document.AddEventListener("DOMContentLoaded", () => {
+  const gallery = document.querySelector(".gallery");
+
+  if (!gallery) return;
+
+  fetch("/assets/gallery/gallery.json")
+    .then((response) => {
+        throw new Error('Failed to load gallery.json!'
+    }
+    return response.json();
+  })
+  .then((items) => {
+    items.forEach((item) = {
+      // Create Card
+      const card = document.CreateElement("div");
+      card.classList.add("gallery-item");
+
+      // Add Image
+      const img = document.CreateElement("img");
+      img.src = item.src;
+      img.alt = item.alt;
+      img.loading = "lazy";
+
+      card.appendChild(img);
+      
+
+          
