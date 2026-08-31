bfbef09851af4c44181ea2b83d4ef6c9ae4f15e9
Lunar Transit <Thomas.roberts23@outlook.com>
2026-08-31 17:29:16 +1000

    added text info below

--- js/gallery.js
+++ js/gallery.js
@@ -26,16 +26,34 @@ document.addEventListener("DOMContentLoaded", () => {
         img.classList.add(item.class);
       }
       
+      // Date and Time for Lightbox
+      if (item.date) img.dataset.date = item.date;
+      if (item.time) img.dataset.time = item.time;
+
       card.appendChild(img);
+
+      // Text Under Image
+      const info = document.createElement("div");
+      info.classList.add("card-info");
+
+      let timeText = "";
+      if (item.date && item.time) {
+        timeText = `${item.date} | ${item.time}`;
+      } else if (item.date) {
+        timeText = item.date;
+      }
+
+      info.innerHTML = `
+        <p class="card-title">${item.alt || ""}</p>
+        ${time.text}
+      `;
+
+      card.AppendChild(info);
       
       // Album Check
       if (item.album) {
         card.classList.add("albumItem");
         card.dataset.album = item.album;
-
-        if (item.date) img.dataset.date = item.date;
-        if (item.time) img.dataset.time = item.time;
-        
       } else {
         card.classList.add("StandardItem");
       }
